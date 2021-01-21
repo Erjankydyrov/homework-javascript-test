62098f200a180f26334bd04d506ed19813833ca2
Erjankydyrov <Erjankydyrov29@gmail.com>
2021-01-21 19:03:30 +0600

    added question1

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // variant1
 
 const points = 0;
+let allPoints
+let question1 = +prompt(`В каком году вышла Warcraft III: The Frozen Throne? Варианты ответов: 1) 1995 2) 2003 3) 1997 Ответ запишите номером варианта.`);
+if (question1 == 2) {
+    allPoints = points + 1;
+}
 
-
-
-alert (`You got ${points} points`)
+alert (`You got ${allPoints} points`);
 
 // variant1
